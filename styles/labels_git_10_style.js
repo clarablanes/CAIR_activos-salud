0bfed9bdee352b5950879a2f5e4ff88bf64f4a55
Clara Blanes <c.blanes@umh.es>
2026-05-29 13:01:41 +0200

    Update labels_git_10_style.js

--- styles/labels_git_10_style.js
+++ styles/labels_git_10_style.js
@@ -6,19 +6,20 @@ var style_labels_git_10 = function(feature, resolution){
         feature: feature,
         variables: {}
     };
-    // Leemos el texto de tu columna 'nombre'
+    
+    // Vamos directos a tu columna 'Label'
     var value = feature.get("Label") !== null ? String(feature.get("Label")) : "";
     
     var style = [ new ol.style.Style({
-        // DIBUJAMOS EL PUNTO (Círculo rojo sólido)
+        // DIBUJAMOS EL PUNTO (Ahora en color NEGRO)
         image: new ol.style.Circle({
             radius: 6.0,
-            fill: new ol.style.Fill({color: 'rgba(255, 0, 0, 1.0)'}),
+            fill: new ol.style.Fill({color: 'rgba(0, 0, 0, 1.0)'}), // <--- Cambiado a negro (0,0,0)
             stroke: new ol.style.Stroke({color: 'rgba(255, 255, 255, 1.0)', width: 2})
         }),
-        // DIBUJAMOS EL TEXTO (Siempre visible arriba del punto)
+        // DIBUJAMOS EL TEXTO (Ahora en BOLD y tamaño 15px)
         text: new ol.style.Text({
-            font: '13px "Open Sans", sans-serif',
+            font: 'bold 15px "Open Sans", sans-serif', // <--- Añadido 'bold' y subido de 13px a 15px
             text: value,
             fill: new ol.style.Fill({color: 'rgba(0, 0, 0, 1.0)'}),
             stroke: new ol.style.Stroke({color: 'rgba(255, 255, 255, 1.0)', width: 3}),
